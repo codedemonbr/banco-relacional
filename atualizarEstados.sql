ec23275bac6278131d29dc51119c65ed044ffe7b
Thiago Henrique <codedemonbr@gmail.com>
2024-05-06 20:10:52 -0300

    primeiros scripts

---
+++ atualizarEstados.sql
@@ -0,0 +1,9 @@
+select est.nome from estados est where sigla = 'MA';
+
+update estados 
+set nome = 'Maranhão'
+where sigla = 'MA';
+
+select est.nome, sigla, populacao from estados est where sigla = 'PR';
+
+update ESTADOS SET nome = 'Paraná', populacao = 11.32 where sigla = 'PR';
--- inserirEstados1.sql
+++ inserirEstados1.sql
@@ -1,14 +1,17 @@
-insert into
-    estados (nome, sigla, regiao, populacao)
-values
-    ('Acre', 'AC', 'Norte', 0.83);
-
-INSERT INTO
-    estados (nome, sigla, regiao, populacao)
-VALUES
-    ('Alagoas', 'AL', 'Nordeste', 3.38),
-    ('Amapá', 'AP', 'Norte', 0.8),
+insert into estados (nome, sigla, regiao, populacao)
+values ('Acre', 'AC', 'Norte', 0.83);
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapa', 'AP', 'Norte', 0.8),
     ('Amazonas', 'AM', 'Norte', 4.06);
-
-
-SELECT ${*} FROM `ESTADOS`;
+SELECT *
+FROM estados;
+use wm;
+ALTER TABLE estados
+modify column regiao ENUM(
+        'Norte',
+        'Nordeste',
+        'Centro-Oeste',
+        'Sudeste',
+        'Sul'
+    );
